4a76e71d6c85748dff504178bd324ea3d9afdda0
DrJume <DrJume@users.noreply.github.com>
2017-08-03 03:07:44 +0200

    See More: - JS Standard Style applied - Updated README

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Installation
+# MorseSimPy
+> Make your Raspberry Pi into a morse device!
+
+## I. Installation of the client
 
 ### 1. Install the dependencies
 ```
@@ -17,6 +20,30 @@ ctl.!default {
 type hw
 card 0
 }
+```
+
+### 3. Connect two buttons to the GPIO Pins
+You can define your own in the Morse.py file. 
+See "pinDit" for the button activating a short tone and "pinDah" for a long tone.
+And the other pin of the button connects to **GROUND**
+
+The server and the client should be on seperate devices. 
+The server will be the connection for multiple clients.
+
+## II. Installation of the server
+
+### 1. Install NodeJS
+Look up the installation instructions for it
+
+### 2. Install the dependencies
+Switch to the server directory and run:
+```
+npm install
+```
+
+### 3. Start the WebSocket server
+```
+node index.js
 ```
 
 ## Links
--- server/index.js
+++ server/index.js
@@ -1,49 +1,48 @@
 // Optional. You will see this name in eg. 'ps' or 'top' command
-process.title = 'node-chat';
+process.title = 'node-morse-sim-py'
 
-var webSocketsServerPort = 8080;
+var webSocketsServerPort = 8080
 
-var webSocketServer = require('websocket').server;
-var http = require('http');
+var WebSocketServer = require('websocket').server
+var http = require('http')
 
 // list of currently connected clients (users)
-var clients = [];
+var clients = []
 
 var server = http.createServer(function (request, response) {
   // Not important for us. We're writing WebSocket server,
   // not HTTP server
-});
+})
 server.listen(webSocketsServerPort, function () {
-  console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
-});
+  console.log((new Date()) + ' Server is listening on port ' + webSocketsServerPort)
+})
 
-
-var wsServer = new webSocketServer({
+var wsServer = new WebSocketServer({
   httpServer: server
-});
+})
 
 wsServer.on('request', function (request) {
-  console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+  console.log((new Date()) + ' Connection from origin ' + request.origin + '.')
 
-  var connection = request.accept(null, request.origin);
+  var connection = request.accept(null, request.origin)
 
-  var index = clients.push(connection) - 1;
+  var index = clients.push(connection) - 1
 
-  console.log((new Date()) + ' ' + connection.remoteAddress + ': Connection accepted.');
+  console.log((new Date()) + ' ' + connection.remoteAddress + ': Connection accepted.')
 
   connection.on('message', function (message) {
     if (message.type === 'utf8') {
       console.log(message.utf8Data)
       // broadcast message to all connected clients
       for (var i = 0; i < clients.length; i++) {
-        if (i == index) continue
-        clients[i].sendUTF(message.utf8Data);
+        if (i === index) continue
+        clients[i].sendUTF(message.utf8Data)
       }
     }
-  });
+  })
   connection.on('close', function () {
-    console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.')
 
-    clients.splice(index, 1);
-  });
-});
+    clients.splice(index, 1)
+  })
+})
